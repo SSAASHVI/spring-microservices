639eb221243780fa1d9e4e8f9ced988680711586
ibmpariwesh <pariweshg@gmail.com>
2022-05-24 15:47:49 +0530

    Resr template

--- demo-1/src/main/java/com/example/demo/OrderService.java
+++ demo-1/src/main/java/com/example/demo/OrderService.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
+	
+	RestTemplate template =new RestTemplate();
 	@Autowired
 	OrderRepository repository;
 	public void saveOrder(OrderVO orderVO) {
 		repository.save(orderVO);
+		template.getForEntity("http://localhost:8080/email?address=ram@gmail.com", String.class);
+		
 		System.out.println(orderVO.getItem());
 		System.out.println(orderVO.getQuantity());
 	}
